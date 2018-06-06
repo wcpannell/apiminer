7eb78b8461e24ad0870d2202a5ff8cf6626657f0
militancy <getmilitancy@gmail.com>
2018-06-05 21:23:08 -0500

    Refactored ClaymoreRPC response dict

--- apiminer/ClaymoreRPC.py
+++ apiminer/ClaymoreRPC.py
@@ -19,6 +19,11 @@ class ClaymoreRPC(object):
     port : int
         The port on which the api is listening
 
+    Attributes
+    ----------
+    response : dict
+        Private storage of formatted response. set by
+        :meth:`ClaymoreRPC.update`
     """
     def __init__(self, ip, port):
         self.ip = str(ip).encode('utf-8')
@@ -26,58 +31,48 @@ class ClaymoreRPC(object):
         self.port = int(port)
 
         #: bool: Set by :meth:`ClaymoreRPC._connect`
-        self.connected = False
+        self._connected = False
 
-        #: str: Miner Version
-        self.version = ''
-
-        #: int: Miner Runtime
-        self.runtime = 0
-
-        #: int: Miner Total Hashrate
-        self.total_hashrate = 0
-
-        #: int: Number of accepted shares
-        self.accepted_shares = 0
-
-        #: int: Number of rejected shares
-        self.rejected_shares = 0
-
-        #: list of int: Hashrate per GPU
-        self.percard_hashrate = [0, ]
-
-        #: int: Total Decred Hashrate. Not used in Ethminer
-        self.total_dcr_hashrate = 0
-
-        #: int: Decred Accepted Shares. Not used in Ethminer
-        self.dcr_accepted_shares = 0
-
-        #: int: Decred Rejected Shares. Not used in Ethminer
-        self.dcr_rejected_shares = 0
-
-        #: list of int: Decred hashrate for each GPU. Not used in Ethminer
-        self.percard_dcr_hashrate = [0, ]
-
-        #: list of list of int: [[Temp1, Fan%1], ..., [TempN, Fan%N]]
-        self.tempsfans = [[0, 0], ]
-
-        #: str: Currently Connected Pool. ex. "pool.pool.com:8080"
-        self.pool = ''
-
-        #: int: total invalid shares
-        self.invalid_shares = 0
-
-        #: int: Number of times the pool has switched
-        self.pool_switches = 0
-
-        #: Decred Invalid Shares. Not used in Ethminer
-        self.dcr_invalid_shares = 0
-
-        #: Decred Pool switches. Not used in Ethminer
-        self.dcr_pool_switches = 0
-
-        #: :class:`socket.socket` Object. This communicates with the API Host
+        #: :obj:`socket.socket` Object. This communicates with the API Host
         self.socket = None
+
+        #: bool: Enable to update every time the response attribute is read.
+        self.auto_update = False
+
+        self._response = {
+            'miner': {
+                'version': '',
+                'ip': '',
+                'port': 0,
+                'runtime': 0
+            },
+            'eth_pool': {
+                'pool': '',
+                'pool_switches': 0,
+                'accepted': 0,
+                'rejected': 0,
+                'invalid': 0,
+                'total_hashrate': 0
+            },
+            'dcr_pool': {
+                'pool': 'Not Implemented',
+                'pool_switches': 0,
+                'accepted': 0,
+                'rejected': 0,
+                'invalid': 0,
+                'total_hashrate': 0
+            },
+            'GPUs': {
+                'GPU 0': {
+                    'eth_hashrate': 0,
+                    'total_dcr_hashrate': 0,
+                    'temp': 0,
+                    'fan': 0
+                }
+            }
+        }
+        """dict: Private storage of formatted response. set by
+        :meth:`ClaymoreRPC.update`"""
 
         # Get Data right off the bat. Could be dangerous? I'm open to changing.
         self.update()
@@ -86,16 +81,16 @@ class ClaymoreRPC(object):
         """Connects to our API Host"""
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.ip, self.port))
-        self.connected = True
+        self._connected = True
 
     def _disconnect(self):
         """Disconnects from our API Host"""
         self.socket.close()
-        self.connected = False
+        self._connected = False
 
     def update(self):
         """Update our model with fresh data. Connects if not connected"""
-        if not self.connected:
+        if not self._connected:
             self._connect()
         self.socket.sendall(
             (
@@ -117,65 +112,14 @@ class ClaymoreRPC(object):
             pass
 
         if received:
-            response = json.loads(received.decode('utf-8'))['result']
-
-            self.version = response[0]
-
-            self.runtime = int(response[1])
-
-            [
-                self.total_hashrate,
-                self.accepted_shares,
-                self.rejected_shares
-            ] = [int(val) for val in response[2].split(';')]
-            if self.total_hashrate > 0:
-                self.total_hashrate /= 1000
-
-            self.percard_hashrate = [
-                (int(val) / 1000) for val in response[3].split(';')
-            ]
-
-            [
-                self.total_dcr_hashrate,
-                self.dcr_accepted_shares,
-                self.dcr_rejected_shares,
-            ] = [
-                int(val) if val != 'off' else -1
-                for val in response[4].split(';')
-            ]
-            if self.total_dcr_hashrate > 0:
-                self.total_dcr_hashrate /= 1000
-
-            self.percard_dcr_hashrate = [
-                (float(val) / 1000) if val != 'off' else -1
-                for val in response[5].split(';')
-            ]
-
-            tempsfans = response[6].split(';')
-            self.tempsfans = [
-                [int(value), int(tempsfans[index+1])]
-                for index, value in enumerate(tempsfans)
-                if not index % 2
-            ]
-
-            self.pool = response[7]
-
-            [
-                self.invalid_shares,
-                self.pool_switches,
-                self.dcr_invalid_shares,
-                self.dcr_pool_switches
-            ] = [
-                int(val) if val != 'off' else -1
-                for val in response[8].split(';')
-            ]
-
+            self._raw_response = json.loads(received.decode('utf-8'))['result']
         else:
             print('invalid JSON RPC response')
 
         # Close the socket when we're not using it.
         # Let me know if this isn't a good idea
         self._disconnect()
+        return self._response
 
     def restart_miner(self):
         """Sends the miner (API Host) the restart command.
@@ -184,7 +128,7 @@ class ClaymoreRPC(object):
         non-responsive, it is very unlikely that this command will be
         effective.
         """
-        if not self.connected:
+        if not self._connected:
             self._connect()
         self.socket.sendall(
             (
@@ -200,41 +144,72 @@ class ClaymoreRPC(object):
         )
         self._disconnect()
 
-    def getdict(self, update=False):
-        """Returns a dictionary of all the objects, in a nice format"""
-        if update:
+    def _format_response(self):
+        self._response['miner']['version'] = \
+                self._raw_response[0],
+        self._response['miner']['runtime'] = \
+            int(self._raw_response[1])
+
+        [
+            self._response['eth_pool']['total_hashrate'],
+            self._response['eth_pool']['accepted'],
+            self._response['eth_pool']['rejected']
+        ] = [int(val) for val in self._raw_response[2].split(';')]
+        if self._response['eth_pool']['total_hashrate'] > 0:
+            self._response['eth_pool']['total_hashrate'] /= 1000
+
+        [
+            self._response['dcr_pool']['total_hashrate'],
+            self._response['dcr_pool']['accepted'],
+            self._response['dcr_pool']['rejected'],
+        ] = [
+            int(val) if val != 'off' else -1
+            for val in self._raw_response[4].split(';')
+        ]
+        if self._response['dcr_pool']['total_hashrate'] > 0:
+            self._response['dcr_pool']['total_hashrate'] /= 1000
+
+        self._response['eth_pool']['pool'] = self._raw_response[7]
+
+        [
+            self._response['eth_pool']['invalid_shares'],
+            self._response['eth_pool']['pool_switches'],
+            self._response['dcr_pool']['invalid'],
+            self._response['dcr_pool']['pool_switches']
+        ] = [
+            int(val) if val != 'off' else -1
+            for val in self._raw_response[8].split(';')
+        ]
+
+        percard_eth_hashrate = [
+            (int(val) / 1000) for val in self._raw_response[3].split(';')
+        ]
+
+        percard_dcr_hashrate = [
+            (float(val) / 1000) if val != 'off' else -1
+            for val in self._raw_response[5].split(';')
+        ]
+
+        tempsfans = self._raw_response[6].split(';')
+        tempsfans = [
+            [int(value), int(tempsfans[index+1])]
+            for index, value in enumerate(tempsfans)
+            if not index % 2
+        ]
+
+        for gpu in range(len(percard_eth_hashrate)):
+            self._response['GPUs']['GPU {}'.format(gpu)] = {
+                'eth_hashrate': percard_eth_hashrate[gpu],
+                'total_dcr_hashrate': percard_dcr_hashrate[gpu],
+                'temp': tempsfans[gpu][0],
+                'fan': tempsfans[gpu][1]
+            }
+
+    @property
+    def response(self):
+        """Returns a dictionary of the response, in a nice format"""
+        if self.auto_update:
             self.update()
 
-        ourdict = {
-            'miner': {
-                'version': self.version,
-                'ip': self.ip,
-                'port': self.port,
-                'runtime': self.runtime
-            },
-            'eth_pool': {
-                'pool': self.pool,
-                'pool_switches': self.pool_switches,
-                'accepted': self.accepted_shares,
-                'rejected': self.rejected_shares,
-                'invalid': self.invalid_shares,
-                'total_hashrate': self.total_hashrate
-            },
-            'dcr_pool': {
-                'pool': 'Not Implemented',
-                'pool_switches': self.dcr_pool_switches,
-                'accepted': self.dcr_accepted_shares,
-                'rejected': self.dcr_rejected_shares,
-                'invalid': self.dcr_invalid_shares,
-                'total_hashrate': self.total_dcr_hashrate
-            },
-            'GPUs': {}
-        }
-        for gpu in range(len(self.percard_hashrate)):
-            ourdict['GPUs']['GPU {}'.format(gpu)] = {
-                'eth_hashrate': self.percard_hashrate[gpu],
-                'total_dcr_hashrate': self.percard_dcr_hashrate[gpu],
-                'temp': self.tempsfans[gpu][0],
-                'fan': self.tempsfans[gpu][1]
-            }
-        return ourdict
+        self._format_response()
+        return self._response
--- tests/test_ClaymoreRPC.py
+++ tests/test_ClaymoreRPC.py
@@ -4,14 +4,27 @@
 
 import unittest
 from unittest.mock import patch
-from apiminer import ClaymoreRPC
+import apiminer
 
 
 class TestClaymoreRPC(unittest.TestCase):
     """"""
     def test(self):
-        with patch('ClaymoreRPC..socket.
-                   self.assertSetEqual(0, 0)
+        with patch('apiminer.ClaymoreRPC.socket') as mocked_socket:
+            test_response = [
+                '0.14.0',
+                '306',
+                '44414;174;2',
+                '14573;15036;14805',
+                '0;0;0',
+                'off;off;off',
+                '54;33; 58;35; 57;35',
+                'us2.ethermine.org:5555',
+                '0;0;0;0'
+            ]
+            mocked_socket.return_value.recv = test_response
+            RPC = apiminer.ClaymoreRPC('example.com', 80)
+            self.assertSetEqual(RPC._raw_response, test_response)
 
 
 if __name__ == '__main__':
