f34089e0e98271e127127e28599edccf7b19b373
W. Clayton Pannell <clayton.pannell@gmail.com>
2019-04-28 23:18:26 -0500

    Fix typo in ClaymoreRPC
    That's what I get for trusting past me to leave code unbroken.

--- apiminer/ClaymoreRPC.py
+++ apiminer/ClaymoreRPC.py
@@ -4,9 +4,13 @@
 
 This is compatable with both Claymore and Ethminer
 """
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-from builtins import * # noqa 401,403
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+from builtins import *  # noqa 401,403
 import socket
 import json
 
@@ -27,8 +31,9 @@ class ClaymoreRPC(object):
         Private storage of formatted response. set by
         :meth:`ClaymoreRPC.update`
     """
+
     def __init__(self, ip, port):
-        self.ip = str(ip).encode('utf-8')
+        self.ip = str(ip).encode("utf-8")
 
         self.port = int(port)
 
@@ -42,36 +47,31 @@ class ClaymoreRPC(object):
         self.auto_update = False
 
         self._response = {
-            'miner': {
-                'version': '',
-                'ip': '',
-                'port': 0,
-                'runtime': 0
+            "miner": {"version": "", "ip": "", "port": 0, "runtime": 0},
+            "eth_pool": {
+                "pool": "",
+                "pool_switches": 0,
+                "accepted": 0,
+                "rejected": 0,
+                "invalid": 0,
+                "total_hashrate": 0,
             },
-            'eth_pool': {
-                'pool': '',
-                'pool_switches': 0,
-                'accepted': 0,
-                'rejected': 0,
-                'invalid': 0,
-                'total_hashrate': 0
+            "dcr_pool": {
+                "pool": "Not Implemented",
+                "pool_switches": 0,
+                "accepted": 0,
+                "rejected": 0,
+                "invalid": 0,
+                "total_hashrate": 0,
             },
-            'dcr_pool': {
-                'pool': 'Not Implemented',
-                'pool_switches': 0,
-                'accepted': 0,
-                'rejected': 0,
-                'invalid': 0,
-                'total_hashrate': 0
-            },
-            'GPUs': {
-                'GPU 0': {
-                    'eth_hashrate': 0,
-                    'total_dcr_hashrate': 0,
-                    'temp': 0,
-                    'fan': 0
+            "GPUs": {
+                "GPU 0": {
+                    "eth_hashrate": 0,
+                    "total_dcr_hashrate": 0,
+                    "temp": 0,
+                    "fan": 0,
                 }
-            }
+            },
         }
         """dict: Private storage of formatted response. set by
         :meth:`ClaymoreRPC.update`"""
@@ -105,23 +105,14 @@ class ClaymoreRPC(object):
             Password for APIs protected by --api-password. Note that this
             password is sent in cleartext. Blame ethminer.
         """
-        query = {
-            "id": 0,
-            "jsonrpc": "2.0",
-            "method": method
-        }
+        query = {"id": 0, "jsonrpc": "2.0", "method": method}
 
         if password is not None:
-            query['params'] = {"psw": password}
+            query["params"] = {"psw": password}
 
         if not self._connected:
             self._connect()
-        self.socket.sendall(
-            (
-                json.dumps(query)
-                + '\n'
-            ).encode('utf-8')
-        )
+        self.socket.sendall((json.dumps(query) + "\n").encode("utf-8"))
 
     def read(self):
         """Read data from API
@@ -143,15 +134,15 @@ class ClaymoreRPC(object):
         self._disconnect()
 
         if received:
-            return json.loads(received.decode('utf-8'))
+            return json.loads(received.decode("utf-8"))
         else:
-            print('invalid JSON RPC response')
+            print("invalid JSON RPC response")
 
     def update(self):
         """DEPRECATION WARNING. This will be removed in the next release"""
         print("DEPRECATION WARNING. This will be removed in the next release")
         self.write("miner_getstat1")
-        self._raw_response = self.read()['result']
+        self._raw_response = self.read()["result"]
 
     def authorize(self, password):
         """Authenticate connection. Used for APIs that have --api-password set.
@@ -165,7 +156,7 @@ class ClaymoreRPC(object):
         """
         self.write(method="api_authorize", password=password)
         response = self.read()
-        if response['result']:
+        if response["result"]:
             self.authorized = True
         else:
             self.authorized = False
@@ -173,68 +164,68 @@ class ClaymoreRPC(object):
     def getstat1(self):
         """Implementation of the getstats1 method."""
         self.write("getstat1")
-        raw_response = self.read()['result']
+        raw_response = self.read()["result"]
         response = dict()
 
-        response['miner']['version'] = raw_response[0]
-        response['miner']['runtime'] = int(raw_response[1])
+        response["miner"]["version"] = raw_response[0]
+        response["miner"]["runtime"] = int(raw_response[1])
 
         [
-            response['eth_pool']['total_hashrate'],
-            response['eth_pool']['accepted'],
-            response['eth_pool']['rejected']
-        ] = [int(val) for val in raw_response[2].split(';')]
-        if response['eth_pool']['total_hashrate'] > 0:
-            response['eth_pool']['total_hashrate'] /= 1000
+            response["eth_pool"]["total_hashrate"],
+            response["eth_pool"]["accepted"],
+            response["eth_pool"]["rejected"],
+        ] = [int(val) for val in raw_response[2].split(";")]
+        if response["eth_pool"]["total_hashrate"] > 0:
+            response["eth_pool"]["total_hashrate"] /= 1000
 
         [
-            response['dcr_pool']['total_hashrate'],
-            response['dcr_pool']['accepted'],
-            response['dcr_pool']['rejected'],
+            response["dcr_pool"]["total_hashrate"],
+            response["dcr_pool"]["accepted"],
+            response["dcr_pool"]["rejected"],
         ] = [
-            int(val) if val != 'off' else -1
-            for val in raw_response[4].split(';')
+            int(val) if val != "off" else -1
+            for val in raw_response[4].split(";")
         ]
-        if response['dcr_pool']['total_hashrate'] > 0:
-            response['dcr_pool']['total_hashrate'] /= 1000
+        if response["dcr_pool"]["total_hashrate"] > 0:
+            response["dcr_pool"]["total_hashrate"] /= 1000
 
-        response['eth_pool']['pool'] = raw_response[7]
+        response["eth_pool"]["pool"] = raw_response[7]
 
         [
-            response['eth_pool']['invalid'],
-            response['eth_pool']['pool_switches'],
-            response['dcr_pool']['invalid'],
-            response['dcr_pool']['pool_switches']
+            response["eth_pool"]["invalid"],
+            response["eth_pool"]["pool_switches"],
+            response["dcr_pool"]["invalid"],
+            response["dcr_pool"]["pool_switches"],
         ] = [
-            int(val) if val != 'off' else -1
-            for val in raw_response[8].split(';')
+            int(val) if val != "off" else -1
+            for val in raw_response[8].split(";")
         ]
 
         percard_eth_hashrate = [
-            (int(val) / 1000) for val in raw_response[3].split(';')
+            (int(val) / 1000) for val in raw_response[3].split(";")
         ]
 
         percard_dcr_hashrate = [
-            (float(val) / 1000) if val != 'off' else -1
-            for val in raw_response[5].split(';')
+            (float(val) / 1000) if val != "off" else -1
+            for val in raw_response[5].split(";")
         ]
 
-        tempsfans = raw_response[6].split(';')
+        tempsfans = raw_response[6].split(";")
         tempsfans = [
-            [int(value), int(tempsfans[index+1])]
+            [int(value), int(tempsfans[index + 1])]
             for index, value in enumerate(tempsfans)
             if not index % 2
         ]
 
         for gpu in range(len(percard_eth_hashrate)):
-            response['GPUs']['GPU {}'.format(gpu)] = {
-                'eth_hashrate': percard_eth_hashrate[gpu],
-                'dcr_hashrate': percard_dcr_hashrate[gpu],
-                'temp': tempsfans[gpu][0],
-                'fan': tempsfans[gpu][1]
+            response["GPUs"]["GPU {}".format(gpu)] = {
+                "eth_hashrate": percard_eth_hashrate[gpu],
+                "dcr_hashrate": percard_dcr_hashrate[gpu],
+                "temp": tempsfans[gpu][0],
+                "fan": tempsfans[gpu][1],
             }
-        response['miner']['ip'] = self.ip
-        response['miner']['port'] = self.port
+        response["miner"]["ip"] = self.ip
+        response["miner"]["port"] = self.port
 
         return response
 
@@ -289,70 +280,68 @@ class ClaymoreRPC(object):
         """DEPRECATED WARNING. This will be removed in the next release"""
         print(
             "DEPRECATION WARNING. This will be removed in the next release\n",
-            "Formatting will now be a part of each API method implementation"
+            "Formatting will now be a part of each API method implementation",
         )
 
-        self._response['miner']['version'] = \
-            self._raw_response[0]
-        self._response['miner']['runtime'] = \
-            int(self._raw_response[1])
+        self._response["miner"]["version"] = self._raw_response[0]
+        self._response["miner"]["runtime"] = int(self._raw_response[1])
 
         [
-            self._response['eth_pool']['total_hashrate'],
-            self._response['eth_pool']['accepted'],
-            self._response['eth_pool']['rejected']
-        ] = [int(val) for val in self._raw_response[2].split(';')]
-        if self._response['eth_pool']['total_hashrate'] > 0:
-            self._response['eth_pool']['total_hashrate'] /= 1000
+            self._response["eth_pool"]["total_hashrate"],
+            self._response["eth_pool"]["accepted"],
+            self._response["eth_pool"]["rejected"],
+        ] = [int(val) for val in self._raw_response[2].split(";")]
+        if self._response["eth_pool"]["total_hashrate"] > 0:
+            self._response["eth_pool"]["total_hashrate"] /= 1000
 
         [
-            self._response['dcr_pool']['total_hashrate'],
-            self._response['dcr_pool']['accepted'],
-            self._response['dcr_pool']['rejected'],
+            self._response["dcr_pool"]["total_hashrate"],
+            self._response["dcr_pool"]["accepted"],
+            self._response["dcr_pool"]["rejected"],
         ] = [
-            int(val) if val != 'off' else -1
-            for val in self._raw_response[4].split(';')
+            int(val) if val != "off" else -1
+            for val in self._raw_response[4].split(";")
         ]
-        if self._response['dcr_pool']['total_hashrate'] > 0:
-            self._response['dcr_pool']['total_hashrate'] /= 1000
+        if self._response["dcr_pool"]["total_hashrate"] > 0:
+            self._response["dcr_pool"]["total_hashrate"] /= 1000
 
-        self._response['eth_pool']['pool'] = self._raw_response[7]
+        self._response["eth_pool"]["pool"] = self._raw_response[7]
 
         [
-            self._response['eth_pool']['invalid'],
-            self._response['eth_pool']['pool_switches'],
-            self._response['dcr_pool']['invalid'],
-            self._response['dcr_pool']['pool_switches']
+            self._response["eth_pool"]["invalid"],
+            self._response["eth_pool"]["pool_switches"],
+            self._response["dcr_pool"]["invalid"],
+            self._response["dcr_pool"]["pool_switches"],
         ] = [
-            int(val) if val != 'off' else -1
-            for val in self._raw_response[8].split(';')
+            int(val) if val != "off" else -1
+            for val in self._raw_response[8].split(";")
         ]
 
         percard_eth_hashrate = [
-            (int(val) / 1000) for val in self._raw_response[3].split(';')
+            (int(val) / 1000) for val in self._raw_response[3].split(";")
         ]
 
         percard_dcr_hashrate = [
-            (float(val) / 1000) if val != 'off' else -1
-            for val in self._raw_response[5].split(';')
+            (float(val) / 1000) if val != "off" else -1
+            for val in self._raw_response[5].split(";")
         ]
 
-        tempsfans = self._raw_response[6].split(';')
+        tempsfans = self._raw_response[6].split(";")
         tempsfans = [
-            [int(value), int(tempsfans[index+1])]
+            [int(value), int(tempsfans[index + 1])]
             for index, value in enumerate(tempsfans)
             if not index % 2
         ]
 
         for gpu in range(len(percard_eth_hashrate)):
-            self._response['GPUs']['GPU {}'.format(gpu)] = {
-                'eth_hashrate': percard_eth_hashrate[gpu],
-                'dcr_hashrate': percard_dcr_hashrate[gpu],
-                'temp': tempsfans[gpu][0],
-                'fan': tempsfans[gpu][1]
+            self._response["GPUs"]["GPU {}".format(gpu)] = {
+                "eth_hashrate": percard_eth_hashrate[gpu],
+                "dcr_hashrate": percard_dcr_hashrate[gpu],
+                "temp": tempsfans[gpu][0],
+                "fan": tempsfans[gpu][1],
             }
-        self._response['miner']['ip'] = self.ip
-        self._response['miner']['port'] = self.port
+        self._response["miner"]["ip"] = self.ip
+        self._response["miner"]["port"] = self.port
 
     @property
     def response(self):
@@ -363,6 +352,7 @@ Returns a dictionary of the response, in a nice format"""
 
         self._format_response()
         return self._response
+
 
 class EthminerRPC(ClaymoreRPC):
     """Class that uses the Ethminer superset of ClaymoreRPC protocol listener
@@ -381,8 +371,9 @@ class EthminerRPC(ClaymoreRPC):
         Private storage of formatted response. set by
         :meth:`ClaymoreRPC.update`
     """
+
     def __init__(self, ip, port):
-        super().__init__(ip, port):
+        super().__init__(ip, port)
 
     def getstatdetail(self):
         """Returns dict of detailed statistical data
